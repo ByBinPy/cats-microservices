45a42078722f72fdb592610e42a10f8f2c4d2d77
ByBinPy <bin.22@inbox.ru>
2024-04-06 00:34:20 +0300

    add cat controller and service

--- lab3/service/src/main/java/org/example/impl/dto/CatDto.java
+++ lab3/service/src/main/java/org/example/impl/dto/CatDto.java
@@ -1,16 +1,20 @@
 package org.example.impl.dto;
 
 import lombok.Data;
+import org.example.implementations.entities.Cat;
 import org.example.implementations.entities.Owner;
 import org.example.implementations.Colors;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
 @Data
 public class CatDto {
-    private Long id;
+    private Integer id;
     private String name;
     private LocalDate dateOfBirth;
     private String breed;
     private Colors color;
     private Owner owner;
+    private Set<Cat> friends;
 }
