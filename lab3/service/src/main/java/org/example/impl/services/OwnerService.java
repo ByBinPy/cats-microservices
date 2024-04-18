1154a04740a218813af49f7a6474131bc9141e36
ByBinPy <bin.22@inbox.ru>
2024-04-18 17:57:35 +0300

    fix OwnerService

--- lab3/service/src/main/java/org/example/impl/services/OwnerService.java
+++ lab3/service/src/main/java/org/example/impl/services/OwnerService.java
@@ -1,9 +1,12 @@
 package org.example.impl.services;
 
+import org.example.declarations.CatDao;
 import org.example.declarations.OwnerDao;
 import org.example.exceptions.SaveExistOwner;
 import org.example.exceptions.UnknownOwner;
+import org.example.impl.dto.CatDto;
 import org.example.impl.dto.OwnerDto;
+import org.example.implementations.entities.Cat;
 import org.example.implementations.entities.Owner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,14 +14,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class OwnerService {
     private final OwnerDao ownerDao;
+    private final CatDao catDao;
 
     @Autowired
-    public OwnerService(OwnerDao ownerDao) {
+    public OwnerService(OwnerDao ownerDao, CatDao catDao) {
         this.ownerDao = ownerDao;
+        this.catDao = catDao;
     }
 
     public ResponseEntity<?> getById(Integer id) {
@@ -27,33 +33,14 @@ public class OwnerService {
             return new ResponseEntity<>(new UnknownOwner(), HttpStatus.NOT_FOUND);
 
         Owner owner = ownerOptional.get();
-        OwnerDto ownerDto = new OwnerDto();
-        ownerDto.setId(owner.getId());
-        ownerDto.setName(owner.getName());
-        ownerDto.setDateOfBirth(owner.getDateOfBirth());
-        ownerDto.setCats(owner.getCats());
 
-        return new ResponseEntity<>(ownerDto, HttpStatus.OK);
+        return new ResponseEntity<>(converOwnerToDto(owner), HttpStatus.OK);
     }
     public ResponseEntity<?> save(OwnerDto ownerDto) {
         if (ownerDao.findById(ownerDto.getId()).isPresent())
             return new ResponseEntity<>(new SaveExistOwner(), HttpStatus.NOT_ACCEPTABLE);
 
-        Owner owner = new Owner();
-        owner.setName(ownerDto.getName());
-        owner.setId(ownerDto.getId());
-        owner.setDateOfBirth(ownerDto.getDateOfBirth());
-        owner.setCats(ownerDto.getCats());
-
-
-        Owner ownerSaved = ownerDao.save(owner);
-
-        ownerDto.setId(ownerSaved.getId());
-        ownerDto.setName(ownerSaved.getName());
-        ownerDto.setDateOfBirth(ownerSaved.getDateOfBirth());
-        ownerDto.setCats(ownerSaved.getCats());
-
-        return new ResponseEntity<>(ownerDto, HttpStatus.CREATED);
+        return new ResponseEntity<>(converOwnerToDto(ownerDao.save(convertDtoToOwner(ownerDto))), HttpStatus.CREATED);
     }
     public void delete(Integer id){
         ownerDao.deleteById(id);
@@ -63,13 +50,22 @@ public class OwnerService {
         if (ownerDao.findById(id).isEmpty())
             return new ResponseEntity<>(new UnknownOwner(), HttpStatus.NOT_FOUND);
 
+        return new ResponseEntity<>(ownerDao.save(convertDtoToOwner(newOwner)), HttpStatus.ACCEPTED);
+    }
+    private OwnerDto converOwnerToDto(Owner owner) {
+        OwnerDto ownerDto = new OwnerDto();
+        ownerDto.setId(owner.getId());
+        ownerDto.setName(owner.getName());
+        ownerDto.setDateOfBirth(owner.getDateOfBirth());
+        ownerDto.setCatIds(owner.getCats().stream().map(Cat::getId).collect(Collectors.toList()));
+        return ownerDto;
+    }
+    private Owner convertDtoToOwner(OwnerDto ownerDto) {
         Owner owner = new Owner();
-        owner.setId(newOwner.getId());
-        owner.setName(newOwner.getName());
-        owner.setDateOfBirth(newOwner.getDateOfBirth());
-        owner.setCats(newOwner.getCats());
-        ownerDao.save(owner);
-
-        return new ResponseEntity<>(newOwner, HttpStatus.ACCEPTED);
+        owner.setName(ownerDto.getName());
+        owner.setId(ownerDto.getId());
+        owner.setDateOfBirth(ownerDto.getDateOfBirth());
+        owner.setCats(ownerDto.getCatIds().stream().map((Integer id) -> catDao.findById(id).get()).collect(Collectors.toList()));
+        return owner;
     }
 }
